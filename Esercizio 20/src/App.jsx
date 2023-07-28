bc9e8c3ed8b1c044180b3c1f238fea4125d5ef40
FabiIb <131887709+FabiIb@users.noreply.github.com>
2023-07-28 11:47:04 +0200

    Update App.jsx

--- Esercizio 20/src/App.jsx
+++ Esercizio 20/src/App.jsx
@@ -1,13 +1,7 @@
 import Login from "./Login";
 function App() {
-  const handleLogin = (e) => {
-    console.log("Log", e);
-  };
   return (
-    <>
-      <h1>Page Login</h1>
-      <Login onLog={handleLogin} />
-    </>
+  <Login  />
   );
 }
 
