bfe6bb3e074055e0c7529dba1578f8b5dbd7df0b
FabiIb <ibrahimifabiola9@gmail.com>
2023-08-03 17:22:37 +0200

    new_exe

--- Esercizio 29/src/App.jsx
+++ Esercizio 29/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import DisplayLanguage from "./DisplayLanguage";
+import LenguageContext from "./LenguageContext";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [language, setLanguage] = useState("it")
+  const handleLanguageChange = ((e) => {
+    const data = e.target.value
+    setLanguage(data)
+  })
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <select onChange={handleLanguageChange}>
+        <option value="it">ITALIANO</option>
+        <option value="en">ENGLISH</option>
+      </select>
+      <LenguageContext.Provider value={language}>
+        <DisplayLanguage />
+      </LenguageContext.Provider>
+    </div>
+
+
   )
 }
 
-export default App
+export default App;
+
--- Esercizio 32/src/ClickCounter.jsx
+++ Esercizio 32/src/ClickCounter.jsx
@@ -5,19 +5,17 @@ function ClickCounter({ onCounterChange }) {
     const click = () => {
         setCounter(counter + 1);
     };
-    const clickDecrement = () => {
-        setCounter(counter - 1);
-    };
+
 
     useEffect(() => {
         onCounterChange(counter); // lo utilizzo per svolgere un azione al cambiare degli argomenti degli arrey di dipendenza useEffect(()=>{},[]) <===== il metodo in cui viene scritto, la prima parte è la callback mentre la seconda parte dopo la virgola e l'array delle dipendenze 
-    }, [counter]);
+    }, [counter, onCounterChange]);
 
     return (
         <>
             <p>{counter} </p>
             <button onClick={click}>Click Me</button>
-            <button onClick={clickDecrement}>decrement</button>
+
 
 
         </>
