2aee10fac2295ed48629874c86fd28b03e6bce52
FabiIb <ibrahimifabiola9@gmail.com>
2023-09-06 18:23:34 +0200

    51

--- Esercizio 51/src/UseGitHubUser.jsx
+++ Esercizio 51/src/UseGitHubUser.jsx
@@ -1,18 +1,23 @@
-import useSwr from 'swr'
-function UseGitHubUser({ username }) {
-    const dataFetch = async (url) => {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
-    }
+import useSWR from "swr";
 
-    const { data, error } = useSwr(username ? `https://api.github.com/users${username}` : null, dataFetch)
-
-    return {
-        users: data,
-        error,
-        isLoading: !data && !error
+function UseGitHubUser(username) {
+  async function DataFetch() {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(newError(error));
     }
+  }
+
+  const { data } = useSWR(username ? `https://api.github.com/users/${username}` : null, DataFetch);
+
+  return (
+    <>
+      {!data ? <p>Loading...</p> : <ul>{data.id && <li>{data.id}</li>}</ul>}
+    </>
+  );
 }
 
-export default UseGitHubUser
+export default UseGitHubUser;
